ed0a1408c1c0fd3c39a1da8ac8552d4600f89f42
S-Alexis746 <alexissantucho00@gmail.com>
2024-06-25 14:01:14 -0300

    Update urls.py

--- nasa_image_gallery/urls.py
+++ nasa_image_gallery/urls.py
@@ -1,16 +1,13 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index_page, name='index-page'),
-    path('login/', views.index_page, name='login'),
+    path('registration/login/', views.user_login, name='login'),  # Apuntar a views.user_login
     path('home/', views.home, name='home'),
     path('buscar/', views.search, name='buscar'),
-
     path('favourites/', views.getAllFavouritesByUser, name='favoritos'),
     path('favourites/add/', views.saveFavourite, name='agregar-favorito'),
     path('favourites/delete/', views.deleteFavourite, name='borrar-favorito'),
-
     path('exit/', views.exit, name='exit'),
 ]
